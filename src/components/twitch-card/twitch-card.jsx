b6ef54020ad265f2ba1cc4bfb48c880a2988af6c
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-05 23:45:55 +0530

    feat: write markup and partial styling for twitch card

---
+++ src/components/twitch-card/twitch-card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./twitch-card.css";
+
+const TwitchCard = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-content">
+          <h3 className="card-title">I know exactly what I'm doing</h3>
+          <h4 className="card-subtitle">
+            But in a much more real sense, I have no idea what I'm doing.
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TwitchCard;
