ef5cc13cd7a6c949eca03a828143c8a05ccc87ba
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-06 12:54:20 +0530

    feat: render each word of subtitle as a span

--- src/components/twitch-card/twitch-card.jsx
+++ src/components/twitch-card/twitch-card.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./twitch-card.css";
 
+/* const subtitle = document.getElementsByClassName("card-subtitle")[0];
+
+const createWord = (text, index) => {
+  const word = document.createElement("span");
+
+  word.innerHTML = `${text}`;
+
+  word.classList.add("card-subtitle-word");
+
+  return word;
+};
+
+const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
+
+const createSubtitle = (text) => text.split(" ").map(addWord); */
+
+const subtitle =
+  "But in a much more real sense, I have no idea what I'm doing.";
+
 const TwitchCard = () => {
   return (
     <div className="container">
@@ -8,7 +27,9 @@ const TwitchCard = () => {
         <div className="card-content">
           <h3 className="card-title">I know exactly what I'm doing</h3>
           <h4 className="card-subtitle">
-            But in a much more real sense, I have no idea what I'm doing.
+            {subtitle.split(" ").map((word) => (
+              <span className="card-subtitle-word">{word}</span>
+            ))}
           </h4>
         </div>
       </div>
